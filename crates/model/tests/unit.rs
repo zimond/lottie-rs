00ebaabb6d84b34d6883c8b09172c3f4d64c8a8e
Zimon Dai <daizhuoxian@gmail.com>
2023-10-07 11:08:37 +0800

    test: add a new test to model deser test set

--- crates/model/tests/unit.rs
+++ crates/model/tests/unit.rs
@@ -1,11 +1,11 @@
 use std::fs;
 use std::io::Error;
 
-use lottie_model::{GradientFill, Stroke, TextRange, Transform};
+use lottie_model::{Animated, GradientFill, Stroke, TextRange, Transform, Vector2D};
 
 #[test]
 fn test_transform_complex() -> Result<(), Error> {
-    let file = fs::File::open("../../fixtures/unit/segments/transform_complex.json")?;
+    let file = fs::File::open("../../fixtures/segments/transform_complex.json")?;
     let d = &mut serde_json::Deserializer::from_reader(file);
     let _: Transform = match serde_path_to_error::deserialize(d) {
         Ok(m) => m,
@@ -19,7 +19,7 @@ fn test_transform_complex() -> Result<(), Error> {
 
 #[test]
 fn test_stroke() -> Result<(), Error> {
-    let file = fs::File::open("../../fixtures/unit/segments/stroke.json")?;
+    let file = fs::File::open("../../fixtures/segments/stroke.json")?;
     let d = &mut serde_json::Deserializer::from_reader(file);
     let _: Stroke = match serde_path_to_error::deserialize(d) {
         Ok(m) => m,
@@ -33,7 +33,7 @@ fn test_stroke() -> Result<(), Error> {
 
 #[test]
 fn test_gradient_fill() -> Result<(), Error> {
-    let file = fs::File::open("../../fixtures/unit/segments/gradient_fill.json")?;
+    let file = fs::File::open("../../fixtures/segments/gradient_fill.json")?;
     let d = &mut serde_json::Deserializer::from_reader(file);
     let _: GradientFill = serde_path_to_error::deserialize(d).unwrap();
     Ok(())
@@ -41,9 +41,18 @@ fn test_gradient_fill() -> Result<(), Error> {
 
 #[test]
 fn test_text_range() -> Result<(), Error> {
-    let file = fs::File::open("../../fixtures/unit/segments/text_range.json")?;
+    let file = fs::File::open("../../fixtures/segments/text_range.json")?;
     let d = &mut serde_json::Deserializer::from_reader(file);
     let d: TextRange = serde_path_to_error::deserialize(d).unwrap();
+    println!("{:?}", d);
+    Ok(())
+}
+
+#[test]
+fn test_legacy_animated_position() -> Result<(), Error> {
+    let file = fs::File::open("../../fixtures/segments/animated_position_legacy.json")?;
+    let d = &mut serde_json::Deserializer::from_reader(file);
+    let d: Animated<Vector2D> = serde_path_to_error::deserialize(d).unwrap();
     println!("{:?}", d);
     Ok(())
 }
