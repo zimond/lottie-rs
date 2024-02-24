e2b5541d9b16cec623d48acf976b07994ab4ce05
biojet1 <biojet1@gmail.com>
2024-02-24 15:59:37 +0800

    Correction for Lerp for Vec<GradientColor>
    should be Rgba::new_u8 not Rgba::new_f32

--- crates/core/src/lerp.rs
+++ crates/core/src/lerp.rs
@@ -66,7 +66,7 @@ impl Lerp for Vec<GradientColor> {
                 let o = y.offset + (x.offset - y.offset) * t;
                 GradientColor {
                     offset: o,
-                    color: Rgba::new_f32(r, g, b, a),
+                    color: Rgba::new_u8(r as u8, g as u8, b as u8, a as u8),
                 }
             })
             .collect()
